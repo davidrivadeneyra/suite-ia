9199288e23ab299a809bd043a8258e8d4c5b1866
David Rivadeneyra <david.rivadeneyra.c@gmail.com>
2024-11-18 10:09:41 -0500

    nuevo tamaño de bigger

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import ScrollSlider from './components/ScrollSlider'
+import MagnetButtons from './MagnetButton'
 
 function App() {
   return (
@@ -9,14 +10,22 @@ function App() {
 		<div className='main-background mb-36'>
 			<header>
 				<div className='main-inner-section h-20 flex justify-between items-center'>
-					<img className='h-4 lg:h-6'  src="/assets/logo.svg" alt="" />
-					<a href="mailto:hello@craftme.agency" className="btn-small">Get it touch</a>
+					<MagnetButtons>
+						<a href="/">
+							<img className='h-4 lg:h-6'  src="/assets/logo.svg" alt="" />
+						</a>
+					</MagnetButtons>
+					<MagnetButtons>
+						<a href="mailto:hello@craftme.agency" className="btn-small">Get it touch</a>
+					</MagnetButtons>
 				</div>
 			</header>
 			<div className="main-inner-section">
 				<h1 className="lg:w-[636px] title-bigger pb-3 lg:pb-6 pt-24">A design partner for your startup</h1>
 				<p className="lg:w-[364px] descriptions pb-8 color-descripcion">From impactful websites to full visual identity, we design for growth and results.</p>
-				<a href="mailto:hello@craftme.agency" className="button-regular mb-24">Get it touch</a>
+				<MagnetButtons>
+					<a href="mailto:hello@craftme.agency" className="button-regular mb-24">Get it touch</a>
+				</MagnetButtons>
 			</div>
 			<ScrollSlider/>
 		</div>
@@ -32,7 +41,9 @@ function App() {
 			<div className='p-8 lg:p-[72px] w-full'>
 				<h1 className="title-big pb-2 lg:pb-8">We help brands stand out online</h1>
 				<p className="descriptions pb-6 lg:pb-8 text-[#586578]">From impactful websites to full visual identity, we design for growth and results.</p>
-				<a href="mailto:hello@craftme.agency" className="button-regular">Get it touch</a>
+				<MagnetButtons>
+					<a href="mailto:hello@craftme.agency" className="button-regular">Get it touch</a>
+				</MagnetButtons>
 			</div>
 			
 		</div>
