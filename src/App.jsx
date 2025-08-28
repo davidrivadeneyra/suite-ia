c335269200269d87882f9e2d930bb52a82c2529b
FabioBaglietto <fabiobaglietto@hotmail.com>
2025-08-28 17:28:59 -0500

    Primer commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,66 +4,21 @@ import MagnetButtons from './MagnetButton'
 
 function App() {
   return (
-    <>
-      <main className="main ">
+    <div>
+      <header className="flex justify-between items-start p-6 bg-white shadow-md h-64">
+        <h1 className="text-2xl font-bold">Atelopus</h1>
 
-		<div className='main-background mb-36'>
-			<header>
-				<div className='main-inner-section h-20 flex justify-between items-center'>
-					<MagnetButtons>
-						<a href="/">
-							<img className='h-4 lg:h-6'  src="/assets/logo.svg" alt="" />
-						</a>
-					</MagnetButtons>
-					<MagnetButtons>
-						<a href="mailto:hello@craftme.agency" className="btn-small">Get it touch</a>
-					</MagnetButtons>
-				</div>
-			</header>
-			<div className="main-inner-section">
-				<h1 className="lg:w-[636px] title-bigger pb-3 lg:pb-6 pt-24">A design partner for your startup</h1>
-				<p className="lg:w-[364px] descriptions pb-8 color-descripcion">From impactful websites to full visual identity, we design for growth and results.</p>
-				<MagnetButtons>
-					<a href="mailto:hello@craftme.agency" className="button-regular mb-24">Get it touch</a>
-				</MagnetButtons>
-			</div>
-			<ScrollSlider/>
-		</div>
-		
-		
+        <nav className="flex  gap-4">
+          <ul className="flex flex-col items-end gap-8">
+            <li><a href="#home" className="text-gray-800 hover:text-orange-500">Home</a></li>
+            <li><a href="#services" className="text-gray-800 hover:text-orange-500">Servicios</a></li>
+            <li><a href="#contact" className="text-gray-800 hover:text-orange-500">Contacto</a></li>
+          </ul>
+        </nav>
 
-		<div className="mx-6 md:mx-auto main-inner bg-white rounded-[40px] lg:rounded-[72px] flex flex-col lg:flex-row gap-0 lg:gap-[72px] ">
-			
-			<div className='m-2 lg:m-6 bg-logos lg:w-full h-72 lg:h-auto rounded-[32px] lg:rounded-[48px]'>
-				<div className=''>
-				</div>
-			</div>
-			<div className='p-8 lg:p-[72px] w-full'>
-				<h1 className="title-big pb-2 lg:pb-8">We help brands stand out online</h1>
-				<p className="descriptions pb-6 lg:pb-8 text-[#586578]">From impactful websites to full visual identity, we design for growth and results.</p>
-				<MagnetButtons>
-					<a href="mailto:hello@craftme.agency" className="button-regular">Get it touch</a>
-				</MagnetButtons>
-			</div>
-			
-		</div>
-
-	</main>
-	
-	
-	<div className="h-96">
-		<div className="main-inner-section">
-				<h1 className="lg:w-[636px] title-bigger pb-3 lg:pb-6 pt-24">Aquí va otra sección</h1>
-				<p className="lg:w-[364px] descriptions pb-8 color-descripcion">From impactful websites to full visual identity, we design for growth and results.</p>
-				<MagnetButtons>
-					<a href="mailto:hello@craftme.agency" className="button-regular mb-24">Get it touch</a>
-				</MagnetButtons>
-			</div>
-	</div>
-	
-
-    </>
-  )
+      </header>
+    </div>
+  );
 }
 
 export default App
