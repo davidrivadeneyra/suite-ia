0741a304d16ae54a4db905407b6386d6a71761d8
FabioBaglietto <fabiobaglietto@hotmail.com>
2025-08-31 13:08:20 -0500

    Ya casi termino el header, falta un 5%

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ function App() {
   return (
     <div className="px-10 pt-10">
 
-      <header className="flex justify-between items-start pb-6 max-h-3xl">
+      <header className="flex justify-between items-start mb-6">
 		{/**LOGO */}
-		<div></div>
-        <h1 className="text-2xl font-bold">Atelopus</h1>
+		<div>        <h1 className="text-2xl font-bold">Atelopus</h1>
+</div>
 			{/* NAVEGADOR*/}
         <nav>
           <ul className="flex flex-col items-end gap-1">
@@ -21,36 +21,36 @@ function App() {
         </nav>
       </header>
 	  <body>
-		<hero className="max-h-full">{/*HERO SECTION */}
-			<section className="grid grid-cols-1 md:grid-cols-[3fr_2fr_1fr] h-[348px] items-stretch">
-				<div className="flex flex-col gap-6 h-full mr-10">
-					<h2 className=" text-6xl font-bold leading-none">Detección de robos en tiempo real. Con IA</h2>
+		<hero className="max-h-full mb-16">{/*HERO SECTION*/}
+			<section className="grid grid-cols-1 md:grid-cols-[1.8fr_1fr] md:grid-rows-2 lg:grid-cols-[2fr_1.3fr_1fr] lg:grid-rows-1 items-stretch mb-9 gap-4">{/** EN CELULAR 1 COLUMNA-EN TABLET 2 ROWS 2 COLS(PERO SOLO UN OBJETO EN LA PRIMERA COL) EN DESK 3 COLS*/}
+				<div className="flex flex-col gap-6 h-full md:col-span-2 md:col-start-1 md:row-start-1 lg:col-span-1 mb-9 pr-0 md:pr-0 lg:pr-5">
+					<h2 className=" text-6xl font-bold leading-none flex justify-start">Detección de robos en tiempo real. Con IA</h2>
 					<p className="text-black">Convierte tus camaras en alertas útiles: Detecta comportamientos sospechosos y recibe alertas con video a tu celular para actuar en segundos.</p>	
 					<div className="flex gap-4">
-						{/**BOTONES */}
-						<button className="bg-black text-white flex-[5] py-3 font-bold rounded-md border-2 ">
+						{/**BOTONES VER BIEN LOS TAMAÑOS FLEX[*]*/}
+						<button className="bg-black text-white md:flex-[4] lg:flex-[5] py-3 font-bold rounded-lg border-2 ">
 							Solicitar demo
 						</button>
-						<button className="bg-white tex-black flex-[3] py-3 font-bold rounded-md border-2">
+						<button className="bg-white tex-black md:felx-[1.5] lg:flex-[3] py-3 font-bold rounded-lg border-2">
 							Ver cómo funciona
 						</button>
 					</div>
-				</div>
-				<div className=" ml-5 mr-5">
+				</div>{/**Imagen */}
+				<div className="md:row-start-2 lg:row-start-auto mb-9">
 					<img src="" alt="Hombre identificado robando" className="rounded-lg shadow-lg max-w-full"/>
-				</div>
-				<div className="bg-white rounded-lg shadow-md p-8 flex flex-col justify-end border">
+				</div>{/**CLIENTES SATISFECHOS */}
+				<div className="bg-white rounded-lg shadow-md pl-6 flex flex-col justify-end item-start border md:row-start-2 lg:row-start-auto mb-9">
 					<p className="text-6xl font-bold mt-4">+12</p>
 					<p className="">Clientes satifechos</p>
 				</div>
 			</section>
 			{/**VIDEO */}
-			<video className="w-full h-full object-cover">
-  					
-  
+			<video className="w-full h-full object-cover mb-">
+  				
 			</video>
 		</hero>
 		<main>
+			{/**ABOUT */}
 			<section className="flex gap-11 max-h-max">
 				<div className="bg-black p-16  rounded-md border-2 flex-[2] flex flex-col gap-7">
 					<h2 className="font-bold text-4xl text-white">¿Qué es Atelopus?</h2>
