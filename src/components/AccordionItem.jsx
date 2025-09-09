866cbd846b6372f7e10174478db73f03cd183a41
David Rivadeneyra <david.rivadeneyra.c@gmail.com>
2025-09-09 13:18:58 -0500

    animación en el acordión

--- src/components/AccordionItem.jsx
+++ src/components/AccordionItem.jsx
@@ -3,10 +3,10 @@ import { useState } from "react";
 export default function AccordionItem({
   title,
   children,
-  titleTextColor = "text-black",   // color de título (y del ícono)
-  borderColor = "border-neutral-300",    // color de borde
+  titleTextColor = "text-black",
+  borderColor = "border-neutral-300",
   className = "",
-  iconSize = ""          // tamaño del ícono (+-)
+  iconSize = ""
 }) {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -15,23 +15,27 @@ export default function AccordionItem({
       {/* Borde superior */}
       <hr className={`${borderColor}`} />
 
-      {/* Botón que abre/cierra */}
+      {/* Botón */}
       <button
         onClick={() => setIsOpen(!isOpen)}
         className="text-left flex w-full justify-between py-6"
       >
-        <h2 className={`title-small ${titleTextColor} ${className}`}>{title}</h2>
-        <span className={`title-small ${titleTextColor} ${iconSize} `}>
+        <h2 className={`title-small ${titleTextColor}`}>{title}</h2>
+        <span className={`title-small ${titleTextColor} ${iconSize}`}>
           {isOpen ? "−" : "+"}
         </span>
       </button>
 
-      {/* Contenido */}
-      {isOpen && (
+      {/* Contenido con animación */}
+      <div
+        className={`transition-all duration-500 ease-in-out overflow-hidden ${
+          isOpen ? "max-h-96 opacity-100" : "max-h-0 opacity-0"
+        }`}
+      >
         <div className="flex flex-col justify-start gap-6 py-6">
           {children}
         </div>
-      )}
+      </div>
     </div>
   );
 }
